70621fe3fe35eb99c210eab295dfe521f7b6e8e6
Victor Bozhuk <Viktor.Bozhuk@lnu.edu.ua>
2021-05-24 02:09:09 +0300

    Connected to azuse cloude dataBase

--- TimeManager/ViewModels/CreateEditGlobalTaskViewModel.cs
+++ TimeManager/ViewModels/CreateEditGlobalTaskViewModel.cs
@@ -57,8 +57,8 @@ namespace TimeManager.ViewModels
             }
             else
             {
-                SelectedStatus = TaskStatuses.FirstOrDefault();
                 Load();
+                SelectedStatus = TaskStatuses.FirstOrDefault();
                 NamePage = Texts.Create;
             }
 
